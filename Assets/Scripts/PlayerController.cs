7091f9fa21113bf094b18c368c8179ce432efecc
Katerina Kolchakovska <katerina.kolchakovska@gmail.com>
2024-05-22 22:00:14 +0200

    Snow  Boarder Game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float torqueAmount = 1f;
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+   
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
+    }
+}
